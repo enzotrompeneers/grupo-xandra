5438d20fc22a2abd201ee71830d0c4c5542b9857
enzotrompeneers <trompeneers@telenet.be>
2018-03-12 11:14:25 +0100

    fixed footer with text and icon, animation from the fixed footer to go to the form, responsive header and dist folder

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,14 @@ import foundation from 'foundation-sites';
 import '../css/style.scss';
 
 
+// START: animation from the fixed footer to go to the form
+$('.arrow-down').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#contact-form').offset().top
+    }, 500);
+});
+// END
+
+
 $(document).foundation()
